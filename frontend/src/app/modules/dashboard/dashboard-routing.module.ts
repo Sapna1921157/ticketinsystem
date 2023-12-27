a27ecc258e9b04db878f887144d579fb1e1c201f
Sapna kumari <sapnak7508@gmail.com>
2023-12-27 14:41:54 +0530

    dashboard module added

--- frontend/src/app/modules/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: "dashboard", 
+  pathMatch: "full"
+},
+
+{  path: 'header', component:HeaderComponent},
+
+{path: 'footer', component:FooterComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/modules/dashboard/dashboard.module.ts
+++ frontend/src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
