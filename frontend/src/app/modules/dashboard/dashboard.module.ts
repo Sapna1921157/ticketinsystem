4753be0799bed350938d38536bc2976c7f63c3f4
Sapna kumari <sapnak7508@gmail.com>
2023-12-28 12:13:17 +0530

    added dashboard

--- frontend/src/app/modules/dashboard/dashboard.module.ts
+++ frontend/src/app/modules/dashboard/dashboard.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeaderModule } from './header/header.module';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from 'src/app/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent
+    DashboardComponent,
+    // FooterComponent
+    // HeaderComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    HeaderModule,
   ]
 })
 export class DashboardModule { }
