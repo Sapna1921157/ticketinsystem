b2f14168fc083a860687b0fd5928150977b9e5c2
Sapna kumari <sapnak7508@gmail.com>
2023-12-29 16:46:23 +0530

    updated app routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { SignupComponent } from './signup/signup.component';
-import { ForgetPasswordComponent } from './forget-password/forget-password.component'; 
+import { AuthGuard } from 'src/auth.guard';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
-import { DashbordComponent } from './modules/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: "login", 
-  pathMatch: "full"
-},
-{ path:'signup', component: SignupComponent },
-{
-  path:'login', component: LoginComponent
-},
+  { path: 'login', component: LoginComponent },
+  {
+    path: '', redirectTo: "dashboard",
+    pathMatch: "full"
+  }, { path: 'recoverpassword/:{id} ', component: RecoverPasswordComponent },
+  {
+    path: "dashboard",
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [AuthGuard]
+  },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent },
 
-{  path: 'forget-password', component:ForgetPasswordComponent},
-
-{path: 'recover-password', component:RecoverPasswordComponent},
-{path: 'dashboard', component:DashbordComponent}
-  
 ];
 
 @NgModule({
