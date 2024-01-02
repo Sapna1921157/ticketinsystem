13870a052c1ba78ac8d388744ff3b2bbb309efc1
ChandniRaj1825 <chandniraj068@gmail.com>
2024-01-02 14:11:10 +0530

    recent changes

--- frontend/src/app/modules/dashboard/projects/projects.component.ts
+++ frontend/src/app/modules/dashboard/projects/projects.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-@Component({
+@Component
+(
+  {
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
-})
-export class ProjectsComponent {
-    constructor(private router:Router){
+  }
+)
+export class ProjectsComponent 
+{
+    constructor(private router:Router)
+    {
   
     }
-    navgation(link:any){
+    navgation(link:any)
+    {
       this.router.navigate([`/${link}`])
     }
+
+  activeLink: string | null = null;
+
+  navigate(link: string) {
+    this.activeLink = link;
+    this.router.navigate([`/${link}`]);
+  }
 }
